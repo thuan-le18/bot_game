35d6ab3d3fdd95bdf4723d75dd2b97c2ffac9619
thuan-le18 <142200947+thuan-le18@users.noreply.github.com>
2025-03-08 16:19:53 +0700

    Update bottx.py

--- bottx.py
+++ bottx.py
@@ -20,7 +20,7 @@ db = mysql.connector.connect(
     host=os.environ.get('DB_HOST', '127.0.0.1'),
     user=os.environ.get('DB_USER', 'root'),
     password=os.environ['azglkhsq_bot_game'],
-    database=os.environ['azglkhsq_bot_game'],
+    password = os.environ.get('DB_PASS', 'defaultpassword'),
     port=int(os.environ.get('DB_PORT', 3306))  # Mặc định cổng 3306
 )
 cursor = db.cursor()
