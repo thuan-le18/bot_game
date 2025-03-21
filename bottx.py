ff598ed006e5803f380739a440df9fa258706175
thuan-le18 <142200947+thuan-le18@users.noreply.github.com>
2025-03-21 15:52:36 +0700

    Update bottx.py

--- bottx.py
+++ bottx.py
@@ -221,8 +221,7 @@ async def start_cmd(message: types.Message):
 
         await message.answer(
             f"⚠️ Tài khoản Mega6casino của bạn đã bị khóa vì vi phạm quy định.\n"
-            f"💰 Số dư tài khoản của bạn: {formatted_balance} VNĐ.\n"
-            f"Để mở khóa, vui lòng liên hệ hỗ trợ.",
+            f"💰 Số dư hiện tại của bạn:{formatted_balance} VNĐ.Để mở khóa, vui lòng liên hệ hỗ trợ\n",
             reply_markup=types.ReplyKeyboardRemove()  # Xóa toàn bộ nút
         )
         return
